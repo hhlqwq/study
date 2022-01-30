01b1ffedb36d59ea24ef9c578a75e36efa291f59
hhlqwq <845367567@qq.com>
2022-01-30 10:16:20 +0800

    add debug code

--- test.cpp
+++ test.cpp
@@ -4,5 +4,16 @@ using namespace std;
 int main()
 {
     cout << "test 123" << endl;
+
+    int x = 5;
+    for(int i = 0; i < 10; i++)
+    {
+        x += i;
+        if(x > 15)
+        {
+            x++;
+        }
+    }
+    cout << "x: " << x << endl;
     return 0;
 }
